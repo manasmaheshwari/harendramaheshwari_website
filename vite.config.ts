3628ab8dd671769692a9b6fc142cf339e403ae66
Manas Maheshwari <manas.maheshwari303@gmail.com>
2026-06-16 19:23:06 +0530

    Enable nitro deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
+// import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
+// export default defineConfig({
+//   tanstackStart: {
+//     server: { entry: "server" },
+//   },
+//   vite: {
+//     server: {
+//       allowedHosts: ["all"],
+//     },
+//   },
+// });
+
+//chatgpt improvisiation to deploy on vercel
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: true,
+
   tanstackStart: {
     server: { entry: "server" },
   },
+
   vite: {
     server: {
       allowedHosts: ["all"],
